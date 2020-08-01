3c283eadf7c5d3c4f130f030190c833fe1ee236d
GAnjali <GAnjali@github.com>
2020-08-01 20:06:10 +0530

    Add tests for UpdateNote API

--- server/__test__/noteapi.test.js
+++ server/__test__/noteapi.test.js
@@ -17,8 +17,6 @@ describe('Test the File endpoints', () => {
         });
 
         it("should get response as Notes found with status code 200 when you insert note in db", async () => {
-            console.log("pool1");
-            console.log(pool);
             await pool.query("insert into notes(title,content) values('title', 'content')").then(async () => {
                 const res = await request(app).get('/notes');
                 expect(res.statusCode).toEqual(200);
@@ -43,18 +41,44 @@ describe('Test the File endpoints', () => {
         });
 
         it("should get response status 400 given invalid id", async () => {
-                const res = await request(app).get('/notes/&');
-                expect(res.statusCode).toEqual(400);
-                expect(res.body.message).toEqual("Please input a valid numeric value");
+            const res = await request(app).get('/notes/&');
+            expect(res.statusCode).toEqual(400);
+            expect(res.body.message).toEqual("Please input a valid numeric value");
         });
     });
 
     describe("Testing the addNote API", () => {
         it("should get response with 201 when called addNote with note", async () => {
-            const newNote = {title:'title', content:'content'};
+            const newNote = {title: 'title', content: 'content'};
             const res = await request(app).post('/notes').send(newNote);
             expect(res.status).toEqual(201);
             expect(res.body.message).toEqual("Note created!");
+        });
+    });
+
+    describe("Testing the updateNote API", () => {
+        it("should get response with 200 when called updateNote with note", async () => {
+            const newNote = {id: 1, title: 'title', content: 'content'};
+            await pool.query(`INSERT INTO notes(id,title,content) VALUES('${newNote.id}','${newNote.title}','${newNote.content}')`).then(async () => {
+                const noteToUpdate = {id: 1, title: 'updateTitle', content: 'updateContent'};
+                const res = await request(app).put('/notes/1').send(noteToUpdate);
+                expect(res.statusCode).toEqual(200);
+                expect(res.body.message).toEqual("Note updated!");
+            });
+        });
+
+        it("should get response with 404 when called updateNote with id which does not exist in notes", async () => {
+            const noteToUpdate = {id: 1, title: 'updateTitle', content: 'updateContent'};
+            const res = await request(app).put('/notes/1').send(noteToUpdate);
+            expect(res.statusCode).toEqual(404);
+            expect(res.body.message).toEqual("Cannot find Note with id: 1");
+        });
+
+        it("should get response with 404 when called updateNote with invalid note id as param", async () => {
+            const noteToUpdate = {id: 'A', title: 'updateTitle', content: 'updateContent'};
+            const res = await request(app).put('/notes/A').send(noteToUpdate);
+            expect(res.statusCode).toEqual(400);
+            expect(res.body.message).toEqual("Please input a valid numeric value");
         });
     });
 });
