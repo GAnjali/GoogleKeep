0301a0847d99ca483b919bd3a5014c861d9493ca
GAnjali <GAnjali@github.com>
2020-08-18 19:26:35 +0530

    Fix CORS issues for server API

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {getAllNotes, getOneNote, addNote, updateNote, deleteNote} from './api/controller/notecontroller'
+import {getAllNotes, getOneNote, addNote, updateNote, deleteNote} from './api/controller/notecontroller';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 process.env.NODE_ENV = "development";
 
 app.get('/notes', getAllNotes);
